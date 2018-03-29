341c5759812262037087f717b7bdd64eb954b094
SamsonPaulJr <hello@samsonpaul.me>
2018-03-29 21:00:55 +0200

    adds google auth, updating user image picker

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-
 import 'package:flutter/foundation.dart';   
-
 import 'package:flutter/cupertino.dart';    
+import 'package:google_sign_in/google_sign_in.dart';
+import 'dart:async'; 
 
 void main() {
   runApp(new FriendlychatApp());
@@ -71,7 +71,15 @@ class ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin {
     for (ChatMessage message in _messages)                           //new
       message.animationController.dispose();                         //new
     super.dispose();                                                 //new
-  }     
+  } 
+  Future<Null> _ensureLoggedIn() async {
+    GoogleSignInAccount user = googleSignIn.currentUser;
+    if (user == null)
+      user = await googleSignIn.signInSilently();
+    if (user == null) {
+      await googleSignIn.signIn();
+    }
+  }    
   Widget _buildTextComposer() {
     return new IconTheme(
       data: new IconThemeData(color: Theme.of(context).accentColor),
@@ -112,11 +120,26 @@ class ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin {
       ),
     );
   }
-  void _handleSubmitted(String text) {
+  Future<Null> _handleSubmitted(String text) async {
     _textController.clear();
     setState(() {                                                    //new
       _isComposing = false;                                          //new
-    });                                                              //new
+    });
+    await _ensureLoggedIn();                                       //new
+    _sendMessage(text: text);                                                              //new
+    ChatMessage message = new ChatMessage(
+      text: text,
+      animationController: new AnimationController(
+        duration: new Duration(milliseconds: 700),
+        vsync: this,
+      ),
+    );
+    setState(() {
+      _messages.insert(0, message);
+    });
+    message.animationController.forward();
+  }
+  void _sendMessage({ String text }) {
     ChatMessage message = new ChatMessage(
       text: text,
       animationController: new AnimationController(
@@ -135,36 +158,40 @@ class ChatMessage extends StatelessWidget {
   ChatMessage({this.text, this.animationController});
   final String text;
   final AnimationController animationController;
-  @override
+
   Widget build(BuildContext context) {
-    return new SizeTransition(                                    //new
-    sizeFactor: new CurvedAnimation(                              //new
-        parent: animationController, curve: Curves.easeOut),      //new
-    axisAlignment: 0.0,                                           //new
-    child: new Container(                                    //modified
-      margin: const EdgeInsets.symmetric(vertical: 10.0),
-      child: new Row(
+    return new SizeTransition(
+      sizeFactor: new CurvedAnimation(
+          parent: animationController, curve: Curves.easeOut),
+      axisAlignment: 0.0,
+      child: new Container(
+        margin: const EdgeInsets.symmetric(vertical: 10.0),
+        child: new Row(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
             new Container(
-              margin: const EdgeInsets.only(right: 16.0),
-              child: new CircleAvatar(child: new Text(_name[0])),
-            ),
-            new Expanded(                                               //new
-              child: new Column(                                   //modified
+                margin: const EdgeInsets.only(right: 16.0),
+                child: new CircleAvatar(
+                    backgroundImage:
+                        new NetworkImage(googleSignIn.currentUser.photoUrl)
+                    )
+                ),
+            new Expanded(
+              child: new Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
-                  new Text(_name, style: Theme.of(context).textTheme.subhead),
+                  new Text(googleSignIn.currentUser.displayName,                 //modified
+                      style: Theme.of(context).textTheme.subhead),
                   new Container(
                     margin: const EdgeInsets.only(top: 5.0),
                     child: new Text(text),
                   ),
                 ],
               ),
-            ),    
+            ),
           ],
         ),
-      )                                                           //new
+      ),                                                           //new
     );
   }
 }
@@ -181,3 +208,5 @@ final ThemeData kDefaultTheme = new ThemeData(
   primarySwatch: Colors.purple,
   accentColor: Colors.orangeAccent[400],
 );
+
+final googleSignIn = new GoogleSignIn();  
